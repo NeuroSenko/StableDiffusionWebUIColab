24e83a453fae305c08c017c8b5b8687aed143427
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-09-13 16:37:13 +0500

    обновлено 13.09.2023 в 16:37:13

--- src/gradio_strings.py
+++ src/gradio_strings.py
@@ -5,8 +5,8 @@ import requests
 MESSAGING_API_ENDPOINT = "https://api.gradio.app/gradio-messaging/rus"
 
 en = {
-    "RUNNING_LOCALLY": "Ссылка на градио:",
-    "RUNNING_LOCALLY_SEPARATED": "Ссылка на градио:",
+    "RUNNING_LOCALLY": "сервер поднят",
+    "RUNNING_LOCALLY_SEPARATED": "сервер поднят",
     "SHARE_LINK_DISPLAY": "{}",
     "COULD_NOT_GET_SHARE_LINK": "\nПроблемы с интернетом, ссылку создать не получилось. Используй одну из ссылок выше, например на ngrok\n",
     "COLAB_NO_LOCAL": "Не получилось отобразить локальный интерфейс в колабе, но ссылка создана.",
